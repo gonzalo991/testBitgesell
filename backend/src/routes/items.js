e9cda18f7f7bedbce2f9eb207b93af01d62d9835
Gonzalo Araya <gonxaraya991@gmail.com>
2025-08-19 23:30:43 -0300

    made async some function at items.js to handle readData function, deleted the limit at the route into DataContext.js and improved the stats calculation at stats.js

--- backend/src/routes/items.js
+++ backend/src/routes/items.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const fs = require('fs').promises;
+const fs = require('fs').promises; // used a modern approach to use async functions with fs
 const path = require('path');
 const router = express.Router();
 const DATA_PATH = path.join(__dirname, '../../../data/items.json');
 
 // Utility to read data (intentionally sync to highlight blocking issue)
-async function readData() {
+async function readData() { // I have changed this to an async function to read files asynchronously
+  // Used try/catch block to handle errors properly
   try {
-    const raw = await fs.readFile(DATA_PATH, "utf-8");
+    const raw = await fs.readFile(DATA_PATH, "utf-8"); // I have changed the readFileSync to async function readFile
     return JSON.parse(raw);
   } catch (error) {
     console.error(`There was an error reading file: ${error}`);
@@ -18,7 +19,7 @@ async function readData() {
 // GET /api/items
 router.get('/', async (req, res, next) => {
   try {
-    const data = await readData();
+    const data = await readData(); // i needed to use await here to handle data in the correct way
     const { limit, q } = req.query;
     let results = data;
 
@@ -31,17 +32,17 @@ router.get('/', async (req, res, next) => {
       results = results.slice(0, parseInt(limit));
     }
 
-    res.json(results);
+    res.status(200).json(results);
   } catch (err) {
     next(err);
   }
 });
 
 // GET /api/items/:id
-router.get('/:id', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
-    const data = readData();
-    const item = data.find(i => i.id === parseInt(req.params.id));
+    const data = await readData();
+    const item = await data.find(i => i.id === parseInt(req.params.id));
     if (!item) {
       const err = new Error('Item not found');
       err.status = 404;
@@ -54,11 +55,11 @@ router.get('/:id', (req, res, next) => {
 });
 
 // POST /api/items
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
     // TODO: Validate payload (intentional omission)
     const item = req.body;
-    const data = readData();
+    const data = await readData();
     item.id = Date.now();
     data.push(item);
     fs.writeFileSync(DATA_PATH, JSON.stringify(data, null, 2));
--- backend/src/routes/stats.js
+++ backend/src/routes/stats.js
@@ -4,20 +4,44 @@ const path = require('path');
 const router = express.Router();
 const DATA_PATH = path.join(__dirname, '../../data/items.json');
 
+// Variables to cache status
+let cachedStats = null; // this will save the object with the calculated statistics
+let lastCalculationStats = 0; // This will save the last time the data has been calculated
+const CACHE_DURATION_MS = 10 * 60 * 1000; // Time interval for calculation
+
+const calculateStats = (items) => {
+  const total = items.length;
+  const averagePrice = items.reduce((acc, cur) => acc + cur.price, 0) / items.length;
+  return { total, averagePrice };
+}
+
 // GET /api/stats
 router.get('/', (req, res, next) => {
-  fs.readFile(DATA_PATH, (err, raw) => {
-    if (err) return next(err);
+  const now = Date.now();
 
-    const items = JSON.parse(raw);
-    // Intentional heavy CPU calculation
-    const stats = {
-      total: items.length,
-      averagePrice: items.reduce((acc, cur) => acc + cur.price, 0) / items.length
-    };
+  if (cachedStats && (now - lastCalculationStats) < CACHE_DURATION_MS) {
+    console.log(`Serving cache stats: ${cachedStats}`);
+    return res.json(cachedStats);
+  }
 
-    res.json(stats);
-  });
+  console.log(`Expired Cache or it does not exists. Recalculating...`);
+
+  try {
+    // If we recalculate the stats we save the result of the operation at cachedStats
+    cachedStats = fs.readFile(DATA_PATH, (err, raw) => {
+      if (err) return next(err);
+
+      const items = JSON.parse(raw);
+      // Intentional heavy CPU calculation
+      const stats = calculateStats(items);
+      return stats;
+    });
+    // We updated the schedule at which the stats were calculated
+    lastCalculationStats = Date.now();
+    res.json(cachedStats) // Return the new value of the operation
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
